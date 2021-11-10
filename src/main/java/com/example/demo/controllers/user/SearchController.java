86c7d920f5e167d030e0f2227af754b2c6c8df28
Ilya Yakubov <i.yakubov.pro@gmail.com>
2021-11-10 13:19:37 +0300

    Добавлена функциональность поиска товара по имени

---
+++ src/main/java/com/example/demo/controllers/user/SearchController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controllers.user;
+
+import com.example.demo.model.ImageUtil;
+import com.example.demo.model.Item;
+import com.example.demo.repository.ItemDAO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.LinkedList;
+
+@Controller
+public class SearchController {
+
+    @GetMapping("/search")
+    public String editOrder(@RequestParam("search") String search, Model model) {
+        LinkedList<Item> items = new LinkedList<>();
+        for (Item item : ItemDAO.INSTANCE.getItems()) {
+            if (search.equals(item.getName())) {
+                items.add(item);
+            }
+        }
+        model.addAttribute("items", items);
+        model.addAttribute("imgUtil", new ImageUtil());
+        return "index";
+    }
+}
