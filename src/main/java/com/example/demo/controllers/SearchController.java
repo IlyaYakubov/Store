93e7acf2a6f824b08b94923a0f014c324c295cc2
Ilya Yakubov <i.yakubov.pro@gmail.com>
2021-12-02 20:12:16 +0300

    Добавлен поиск по наименованию товара

--- src/main/java/com/example/demo/controllers/SearchController.java
+++ src/main/java/com/example/demo/controllers/SearchController.java
@@ -1,26 +1,21 @@
 package com.example.demo.controllers;
 
-import com.example.demo.models.Item;
+import com.example.demo.repositories.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.LinkedList;
-
 @Controller
 public class SearchController {
 
+    @Autowired
+    private ItemRepository itemRepository;
+
     @GetMapping("/search")
-    public String editOrder(@RequestParam("search") String search, Model model) {
-        LinkedList<Item> items = new LinkedList<>();
-        /*for (Item item : ItemDAO.INSTANCE.getItems()) {
-            if (search.equals(item.getName())) {
-                items.add(item);
-            }
-        }*/
-        model.addAttribute("items", items);
-        //model.addAttribute("imgUtil", new ImageUtil());
+    public String findItems(@RequestParam("search") String search, Model model) {
+        model.addAttribute("items", itemRepository.findItemByName(search));
         return "index";
     }
 }
