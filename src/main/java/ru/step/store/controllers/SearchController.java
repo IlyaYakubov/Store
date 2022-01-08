f713501380f3dfce1ee6830df6ac76238df1d97f
Ilya Yakubov <i.yakubov.pro@gmail.com>
2022-01-08 17:27:19 +0300

    Добавлен поиск по бренду и цвету и пагинация при просмотре найденного

--- src/main/java/ru/step/store/controllers/SearchController.java
+++ src/main/java/ru/step/store/controllers/SearchController.java
@@ -1,15 +1,24 @@
 package ru.step.store.controllers;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import ru.step.store.models.Brand;
+import ru.step.store.models.Item;
+import ru.step.store.models.Role;
 import ru.step.store.models.User;
-import ru.step.store.repositories.CategoryRepository;
+import ru.step.store.repositories.BrandRepository;
+import ru.step.store.repositories.ColorRepository;
 import ru.step.store.repositories.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collections;
+import java.util.stream.IntStream;
 
 @Controller
 public class SearchController {
@@ -18,31 +27,32 @@ public class SearchController {
     private ItemRepository itemRepository;
 
     @Autowired
-    private CategoryRepository categoryRepository;
+    private BrandRepository brandRepository;
+
+    @Autowired
+    private ColorRepository colorRepository;
 
     @GetMapping("/search")
-    public String findItems(@RequestParam("search") String search, Model model, @AuthenticationPrincipal User user) {
-        model.addAttribute("items", itemRepository.findAll());
-        if (!search.isEmpty()) {
-            model.addAttribute("items", itemRepository.findItemByName(search));
+    public String findItems(@RequestParam(value = "search") String search,
+                            Model model,
+                            @AuthenticationPrincipal User user,
+                            @RequestParam(value = "page", required = false, defaultValue = "0") Integer page) {
+        Page<Item> items = itemRepository.findItemsByName(search, PageRequest.of(page, 3));
+        if (items.getContent().size() == 0) {
+            items = itemRepository.findItemsByBrand(brandRepository.findBrandByName(search), PageRequest.of(page, 3));
         }
-        model.addAttribute("user", "anonymous");
-
-        if (user != null) {
-            model.addAttribute("user", user.getUsername());
+        if (items.getContent().size() == 0) {
+            items = itemRepository.findItemsByColor(colorRepository.findColorByName(search), PageRequest.of(page, 3));
         }
-        return "index";
-    }
 
-    @GetMapping("/search/{categoryId}")
-    public String filter(@PathVariable Long categoryId, Model model, @AuthenticationPrincipal User user) {
-        model.addAttribute("items", itemRepository.findItemByCategory_Id(categoryId));
-        model.addAttribute("categories", categoryRepository.findAll());
+        model.addAttribute("items", items.getContent());
+        model.addAttribute("pages", IntStream.range(0, items.getTotalPages()).toArray());
+        model.addAttribute("search", search);
 
         model.addAttribute("user", "anonymous");
         if (user != null) {
             model.addAttribute("user", user.getUsername());
         }
-        return "index";
+        return "search";
     }
 }
--- src/main/java/ru/step/store/repositories/ItemRepository.java
+++ src/main/java/ru/step/store/repositories/ItemRepository.java
@@ -3,6 +3,8 @@ package ru.step.store.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import ru.step.store.models.Brand;
+import ru.step.store.models.Color;
 import ru.step.store.models.Item;
 
 public interface ItemRepository extends CrudRepository<Item, Long> {
@@ -11,7 +13,11 @@ public interface ItemRepository extends CrudRepository<Item, Long> {
 
     Item findItemByName(String name);
 
-    Item findItemByCategory_Id(Long categoryId);
+    Page<Item> findItemsByName(String name, Pageable pageable);
+
+    Page<Item> findItemsByBrand(Brand brand, Pageable pageable);
+
+    Page<Item> findItemsByColor(Color color, Pageable pageable);
 
     Page<Item> findItemsByCategory_Id(Long categoryId, Pageable pageable);
 
