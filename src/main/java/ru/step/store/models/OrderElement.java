4162881f9d89bd89b3a6bfe1e0825f3c84112189
Ilya Yakubov <i.yakubov.pro@gmail.com>
2022-01-10 14:25:38 +0300

    Добавлен автоинкремент id всех таблиц

--- src/main/java/ru/step/store/models/OrderElement.java
+++ src/main/java/ru/step/store/models/OrderElement.java
@@ -13,7 +13,7 @@ import java.math.BigInteger;
 public class OrderElement {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long quantity;
--- src/main/java/ru/step/store/models/Realization.java
+++ src/main/java/ru/step/store/models/Realization.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 public class Realization {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String phone;
