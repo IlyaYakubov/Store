2724a59bb776b7eefc7e60d2f69c52166fb92e56
Ilya Yakubov <i.yakubov.pro@gmail.com>
2022-01-12 16:33:55 +0300

    Для тестирования

--- src/main/java/ru/step/store/MainApplication.java
+++ src/main/java/ru/step/store/MainApplication.java
@@ -1,5 +1,6 @@
 package ru.step.store;
 
+import org.springframework.context.annotation.ComponentScan;
 import ru.step.store.storage.StorageProperties;
 import ru.step.store.storage.StorageService;
 import org.springframework.boot.CommandLineRunner;
