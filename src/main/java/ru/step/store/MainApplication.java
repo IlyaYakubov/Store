8c4bbd102074a57e557cfb044c3c043edd1e4fbd
Ilya Yakubov <i.yakubov.pro@gmail.com>
2022-01-12 15:43:57 +0300

    Исправлена ошибка

--- src/main/java/ru/step/store/MainApplication.java
+++ src/main/java/ru/step/store/MainApplication.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
-@ComponentScan({"ru.step.store.controllers"})
 public class MainApplication {
 
 	public static void main(String[] args) {
